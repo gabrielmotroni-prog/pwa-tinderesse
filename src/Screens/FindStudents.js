5684260af4fc3be29b1377d3ceca8ba0f166fa34
C1325300 Gabriel Dos Santos Motroni <gabriel.motroni@ibm.com>
2023-04-20 10:36:28 -0300

    24a - logica botoes next e like

--- src/Screens/FindStudents.js
+++ src/Screens/FindStudents.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 //estilo em texto
 import Typography from "@material-ui/core/Typography";
 import StudentCard from "../Components/StudentCard";
 import StudentBrief from "../Components/StudentBrief";
+import mockList from "../mock.json";
 
 const FindStudents = (props) => {
+  //indice da lista de todos
+  const [index, setIndex] = useState(0);
+  //estudante atual na tela
+  const [student, setStudent] = useState();
+  //chaves dos objetos - ids
+  const [keys, setKeys] = useState();
+  //lista de todos
+  const [studentList, setStudentList] = useState();
+
+  //inicializar os estados dos states
+  const initStudentList = () => {
+    const userKeys = Object.keys(mockList);
+    setKeys(userKeys);
+    setStudentList(mockList);
+    setStudent(userKeys[0]);
+  };
+
+  //executar ao objeto montar
+  useEffect(() => {
+    initStudentList();
+  }, []);
+
   const defaultImage = "https://avatars.githubusercontent.com/u/60718584?v=4";
 
+  //atualizar indice
   const nextStudent = () => {
-    console.log("proximo estudante");
+    const nextStudentIndex = index + 1;
+    setIndex(nextStudentIndex);
+    setStudent(keys[nextStudentIndex]);
+    console.log(studentList[student].name);
   };
 
   const handleLike = () => {
-    console.log("gostei");
+    nextStudent();
   };
 
   return (
@@ -25,13 +52,18 @@ const FindStudents = (props) => {
       }}
     >
       <Typography>Encontre estudantes</Typography>
-      <StudentCard
-        image={defaultImage}
-        handleReject={nextStudent}
-        handleLike={handleLike}
-      >
-        <StudentBrief name={"Gabriel"} bio={"Desenvolvedor"}></StudentBrief>
-      </StudentCard>
+      {studentList && studentList[student] && (
+        <StudentCard
+          image={studentList[student].image}
+          handleReject={nextStudent}
+          handleLike={handleLike}
+        >
+          <StudentBrief
+            name={studentList[student].name}
+            bio={studentList[student].bio}
+          ></StudentBrief>
+        </StudentCard>
+      )}
     </div>
   );
 };
